98726aa6d0c639895cdb15f468572d0aefff3ecc
stephskas <steph.sakai@gmail.com>
2022-12-31 14:05:57 -0800

    initial commit for lesson-1-5 saving todoList in localstorage and creating custom hook to retrieve and set todoList

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
+const useSemiPersistentState = () => {
+  const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem("savedTodoList")) || []);
+
+  useEffect(() => {
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList))
+  }, [todoList] )
+
+  return [todoList, setTodoList]
+}
 
 function App() {
-  const [newTodo, setNewTodo] = useState("");
-  const [todoList, setTodoList] = useState([])
-  
-  function addTodo(newTodo){
-    setTodoList([...todoList, newTodo])
+  const [todoList, setTodoList] = (useSemiPersistentState())
+
+  const addTodo = (newTodo) => {
+    setTodoList([...todoList, newTodo]);
   }
+
   return (
    <> 
      <h1>Todo List</h1>
