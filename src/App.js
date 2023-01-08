50e3171c131c56608bdd20f6720fd508cc2dc3a7
Mariana Osorio Lozano <marianaosoriolozano@Marianas-MacBook-Air.local>
2023-01-08 16:41:22 -0600

    todo-list started lesson 1.1

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+const todoList =[{'id': 1, 'tittle': 'Grocery shopping'}, {'id': 2, 'tittle':'pack boxes'}, {'id':3, 'tittle':'buy headphones'}];
+
+function App() {
+  return (
+    <div>
+    <h1>TO-DO List</h1>
+    <ul>
+      {todoList.map((el)=>{
+       return (
+        <li>
+          {`${el.id} ${el.tittle}`}
+          </li>
+       )
+      })}
+    </ul>
+    </div>
+  );
+}
+
+export default App;
