1f8319f87c44c570b2bc4bf566a51cf99b2a76d7
Armanchi <armanijohnson0104@gmail.com>
2023-01-10 10:19:25 -0600

    assignment 1-1

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const todoList = [
+  {
+    id: 1,
+    title: "Complete React assignment",
+  },
+  { id: 2,
+    title: "Do laundry",
+  },
+  {
+    id: 3,
+    title: "walk dog",
+  }
+]
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Todo List</h1>
+
+      <ul>
+        {todoList.map(function(item){
+          return (
+            <li key={item.id}>
+              <span>
+                {item.title}
+              </span>
+            </li>
+          )
+        })}
+      </ul>
+
+    </div>
+  );
+}
+
+export default App;
