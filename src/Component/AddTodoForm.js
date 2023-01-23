f181fcfca10bf2976be427b8ff2fe7315ad11a3e
Said-laasri <saidlaasri3@gmail.com>
2023-01-23 12:02:57 +0100

    Add methode to hundle todos form submitions

---
+++ src/Component/AddTodoForm.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function AddTodoForm(props) {
+  const hundleAddTodo = (e) => {
+    e.preventDefault();
+    const todoTitle = e.target.title.value;
+    console.log(todoTitle);
+    props.onAddTodo(todoTitle);
+    e.target.title.value = "";
+  };
+
+  return (
+    <div>
+      <form onSubmit={hundleAddTodo}>
+        <label htmlFor="todoTitle">Title</label>
+        <input type="text" id="todoTitle" name="title" />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default AddTodoForm;
