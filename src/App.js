26004ad60cb98cdbb6b264118eae254cc423b269
Said-laasri <saidlaasri3@gmail.com>
2023-01-23 11:58:08 +0100

    Use State to set new todos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import AddTodoForm from './AddTodoForm';
-import TodoList from './TodoList';
-import './index.css';
+import React, { useState } from "react";
+import AddTodoForm from "./Component/AddTodoForm";
+import TodoList from "./Component/TodoList";
+import "./index.css";
 
 function App() {
-    return (
-    <div style={{ textAlign: 'center' }}>
+  const [newTodo, setNewTodo] = useState("");
+  const todoList = [
+    { id: 1, title: "Submit Assignement" },
+    { id: 2, title: "Watch video" },
+    { id: 3, title: "Read lesson" },
+  ];
+  return (
+    <div style={{ textAlign: "center" }}>
       <header>
-       <h1>Todo List</h1>
+        <h1>Todo List</h1>
       </header>
-      <AddTodoForm />
-      <TodoList />
-     
+      <AddTodoForm onAddTodo={setNewTodo} />
+      <p
+        style={{
+          fontSize: "26px",
+          fontWeight: "700",
+          color: "rgb(0, 131, 17)",
+        }}
+      >
+        {newTodo}
+      </p>
+      <TodoList todos={todoList} />
     </div>
   );
 }
