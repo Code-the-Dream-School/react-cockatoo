2f381d9e0ea3506e4f3196e199486fe133c18595
stephskas <steph.sakai@gmail.com>
2023-01-26 18:11:36 -0800

    Revert "Merge pull request #9 from stephskas/lesson-1-8"
    This reverts commit 4de9021042264039251ed7e592360b4703e0e447, reversing
    changes made to 781c263953fa6efaa601359fdf48cc5aa6b5062b.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,88 @@
-import React, { useState, useEffect } from 'react';
-import AddTodoForm from './AddTodoForm';
-import TodoList from './TodoList';
+import React, { useState, useEffect } from "react";
+import AddTodoForm from "./AddTodoForm";
+import TodoList from "./TodoList";
 
 const useSemiPersistentState = () => {
-  const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem("savedTodoList")) || []);
+	const [todoList, setTodoList] = useState(
+		JSON.parse(localStorage.getItem("savedTodoList")) || []
+	);
 
-  useEffect(() => {
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList))
-  }, [todoList] )
+	useEffect(() => {
+		localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+	}, [todoList]);
 
-  return [todoList, setTodoList]
-}
+	return [todoList, setTodoList];
+};
 
-function App() {
-  const [todoList, setTodoList] = useSemiPersistentState();
+const useSemiPersistentState = () => {
+	const [todoList, setTodoList] = useState(
+		JSON.parse(localStorage.getItem("savedTodoList")) || []
+	);
 
-  const addTodo = (newTodo) => {
-    setTodoList([...todoList, newTodo]);
-  }
+	useEffect(() => {
+		localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+	}, [todoList]);
 
-  const removeTodo = (id) => {
-    const updatedTodos = todoList.filter(todo => todo.id !== id);
-    setTodoList(updatedTodos);
-  }
+	return [todoList, setTodoList];
+};
 
-  return (
-   <> 
-     <h1>Todo List</h1>
-     <AddTodoForm onAddTodo={addTodo} />
-     <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
-   </>
-  );
-}
+const addTodo = (newTodo) => {
+	setTodoList([...todoList, newTodo]);
+};
+
+const removeTodo = (id) => {
+	const updatedTodos = todoList.filter((todo) => todo.id !== id);
+	setTodoList(updatedTodos);
+};
+
+return (
+	<>
+		<h1>Todo List</h1>
+		<AddTodoForm onAddTodo={addTodo} />
+		<TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+	</>
+);
+
+useEffect(() => {
+	new Promise((resolve, reject) =>
+		setTimeout(() => {
+			resolve({
+				data: {
+					todoList: JSON.parse(localStorage.getItem("savedTodoList")) || [],
+				},
+			});
+		}, 2000)
+	).then((result) => {
+		setTodoList([...result.data.todoList]);
+		setIsLoading(false);
+	});
+}, []);
+
+const addTodo = (newTodo) => {
+	setTodoList([...todoList, newTodo]);
+};
+
+const removeTodo = (id) => {
+	const updatedTodos = todoList.filter((todo) => todo.id !== id);
+	setTodoList(updatedTodos);
+};
+
+useEffect(() => {
+	if (!isLoading) {
+		localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+	}
+}, [todoList]);
+
+return (
+	<>
+		<h1>Todo List</h1>
+		<AddTodoForm onAddTodo={addTodo} />
+		{isLoading ? (
+			<p>Loading...</p>
+		) : (
+			<TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+		)}
+	</>
+);
 
 export default App;
