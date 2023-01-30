912d49fe20722aa0b55b6eb79331291d3f2f9845
Mariana Osorio Lozano <marianaosoriolozano@Marianas-MacBook-Air.local>
2023-01-29 22:34:32 -0600

    Store New Todo in React State

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,16 +1,17 @@
 import React from "react";
 
-function AddTodoForm(){
+function AddTodoForm(props){
    const  handleAddTodo =(event)=>{
     event.preventDefault();
     const todoTitle = event.target.title.value; 
     console.log(todoTitle);
+    props.onAddTodo(todoTitle);
 
     }
 return(
     <form onSubmit={handleAddTodo}>
         <label htmlFor="todoTitle">Tittle</label>
-        <input name="title" id="todoTitle"/>
+        <input type = "text" name="title" id="todoTitle"/>
         <button type="submit">Add</button>
     </form>
 )
--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@
 import './App.css';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
+import { useState } from 'react';
 
 function App() {
+   const [newTodo, setNewTodo] = useState('')
+
   return (
     <div>
     <h1>TO-DO List</h1>
-    <AddTodoForm/>
+    <AddTodoForm onAddTodo={setNewTodo}/>
+    <p>{newTodo}</p>
     <TodoList/>
     </div>
   );
