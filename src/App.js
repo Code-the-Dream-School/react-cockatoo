bb1f78e291976834bf88d2578a5bf8b90f5e22b6
Said-laasri <saidlaasri3@gmail.com>
2023-02-03 16:34:37 +0100

    Move todo List into state

--- src/App.js
+++ src/App.js
@@ -4,27 +4,24 @@ import TodoList from "./Component/TodoList";
 import "./index.css";
 
 function App() {
-  const [newTodo, setNewTodo] = useState("");
-  const todoList = [
-    { id: 1, title: "Submit Assignement" },
-    { id: 2, title: "Watch video" },
-    { id: 3, title: "Read lesson" },
-  ];
+  const [todoList, setTodoList] = useState([]); // [todos, setTodos
+
+  const addTodo = (newTodo) => {
+    setTodoList([...todoList, newTodo]);
+  };
   return (
     <div style={{ textAlign: "center" }}>
       <header>
         <h1>Todo List</h1>
       </header>
-      <AddTodoForm onAddTodo={setNewTodo} />
+      <AddTodoForm onAddTodo={addTodo} />
       <p
         style={{
           fontSize: "26px",
           fontWeight: "700",
           color: "rgb(0, 131, 17)",
         }}
-      >
-        {newTodo}
-      </p>
+      ></p>
       <TodoList todos={todoList} />
     </div>
   );
