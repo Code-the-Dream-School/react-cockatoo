ee95a321422101d8f120959be7b97adcaeefaef2
Lyubov Rodin <luba@lyubovs-mbp.lan>
2023-02-07 02:59:37 -0500

    Lesson 1.4 Lifting State and Props Handling

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 
-const todoList = [
-    {
-      id: 1,
-      title: "Read the book",
-    },
-    {
-      id: 2,
-      title: "Watch video",
-    },
-    {
-      id: 3,
-      title: "Complete assignment",
-    }
-  ]
-function TodoList() {
+// const todoList = [
+//     {
+//       id: 1,
+//       title: "Read the book",
+//     },
+//     {
+//       id: 2,
+//       title: "Watch video",
+//     },
+//     {
+//       id: 3,
+//       title: "Complete assignment",
+//     }
+//   ]
+  // Add props as a parameter to the TodoList functional component
+  export default function TodoList({todoList}) {
   return (
     <ul>
       {todoList.map(function (item) { 
         return (
-          <TodoListItem key={item.id} todo={item} />
+          <TodoListItem key={item.id} title={item.title} />
         )
   //         Inside the map() method, use the TodoListItem component
             //  Pass key as a prop equal to the id of the todo object
@@ -29,4 +30,5 @@ function TodoList() {
     </ul>
   )
 }
-export default TodoList;
+
+
