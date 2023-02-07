02aa8cbffab3c68f2e252b0c176150b8f13f9c72
Said-laasri <saidlaasri3@gmail.com>
2023-02-07 14:46:15 +0100

    Pass todotitle and settodotitle as props from app.js

--- src/Component/AddTodoForm.js
+++ src/Component/AddTodoForm.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 function AddTodoForm(props) {
-  const [todoTitle, setTodoTitle] = useState(""); // [todos, setTodos
-  const { onAddTodo } = props;
+  const { onAddTodo, todoTitle, setTodoTitle } = props;
   const handleTitleChange = (e) => {
     const newTodoTiltle = e.target.value;
     setTodoTitle(newTodoTiltle);
