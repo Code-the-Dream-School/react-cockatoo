4b2b57bcb856a70fd9b117b39b2958dd6e80e02e
stephskas <steph.sakai@gmail.com>
2023-02-10 10:38:43 -0800

    add message if no todos were complete

--- src/components/CompletedTodos.jsx
+++ src/components/CompletedTodos.jsx
@@ -1,17 +1,20 @@
 const TodoList = ({ todoList }) => {
 	const todosCompleted = todoList.filter((todo) => todo.completed === true);
-	console.log('todosCompleted', todosCompleted);
-	return (
-		<>
-			<ol>
-				{todosCompleted.map((todo) => (
-					<li key={todo.id} className='completed-todo'>
-						{todo.title}
-					</li>
-				))}
-			</ol>
-		</>
-	);
+	if (todosCompleted.length > 0) {
+		return (
+			<>
+				<ol>
+					{todosCompleted.map((todo) => (
+						<li key={todo.id} className='completed-todo'>
+							{todo.title}
+						</li>
+					))}
+				</ol>
+			</>
+		);
+	} else {
+		return <h3>You completed nothing.</h3>;
+	}
 };
 
 export default TodoList;
