c520b29837bb473cc68a5ad9d87b368d2aed19bc
Said-laasri <saidlaasri3@gmail.com>
2023-02-15 16:36:14 +0100

    Add onRemoveTodo property

--- src/Component/TodoList.js
+++ src/Component/TodoList.js
@@ -2,12 +2,12 @@ import React from "react";
 import TodoListItem from "./TodoListItem";
 
 const TodoList = (props) => {
-  const { todos } = props;
+  const { todos, onRemoveTodo } = props;
   return (
     <div>
       <ul>
         {todos.map((todo) => (
-          <TodoListItem key={todo.id} todo={todo} />
+          <TodoListItem onRemoveTodo={onRemoveTodo} key={todo.id} todo={todo} />
         ))}
       </ul>
     </div>
