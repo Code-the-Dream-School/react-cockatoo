60007565a12c8c9b59e748f6ac80154583e65377
stephskas <steph.sakai@gmail.com>
2023-02-15 15:07:01 -0800

    refactor code

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 import { TodoContext } from '../context/TodoContext';
-// import PropTypes from 'prop-types';
 
 const TodoContainer = () => {
 	const { isLoading } = useContext(TodoContext);
@@ -13,17 +12,5 @@ const TodoContainer = () => {
 		</>
 	);
 };
-
-// TodoContainer.propTypes = {
-// 	todoList: PropTypes.arrayOf(PropTypes.any),
-// 	isLoading: PropTypes.bool,
-// 	todoListName: PropTypes.string,
-// 	todoTitle: PropTypes.string,
-// 	setTodoTitle: PropTypes.func,
-// 	numberTodos: PropTypes.number,
-// 	onAddTodo: PropTypes.func,
-// 	isMuted: PropTypes.bool,
-// 	setIsMuted: PropTypes.func,
-// };
 
 export default TodoContainer;
