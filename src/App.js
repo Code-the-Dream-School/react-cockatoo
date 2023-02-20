bedb7f0f59c515c504152bc23896eb108cd461eb
Mariana Osorio Lozano <marianaosoriolozano@Marianas-MacBook-Air.local>
2023-02-19 21:46:04 -0600

    custom hook state and fragment

--- src/App.js
+++ src/App.js
@@ -2,21 +2,34 @@ import "./App.css";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 import { useState } from "react";
+import { useEffect } from "react";
+
+const useSemiPersistentState =()=>{
+const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem("savedTodoList")));
+  useEffect(()=>{
+localStorage.setItem('savedTodoList',JSON.stringify(todoList))
+  },[todoList]);
+
+  return [todoList, setTodoList];
+}
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
+
+  const [todoList, setTodoList] = useSemiPersistentState();
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList,newTodo]);
   };
 
+
+
   return (
-    <div>
+    <>
       <h1>TO-DO List</h1>
       <AddTodoForm onAddTodo={addTodo} />
       <p>{addTodo}</p>
       <TodoList todoList={todoList} />
-    </div>
+    </>
   );
 }
 
