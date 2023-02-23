8a22c4d53ae671b1cef42ef6341f10a12c6baceb
Said-laasri <saidlaasri3@gmail.com>
2023-02-23 16:00:35 +0100

    Retrieve data from localstorage using async

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React, { useState, useEffect } from "react";
 import AddTodoForm from "./Component/AddTodoForm";
 import TodoList from "./Component/TodoList";
+import LoadIcon from "./Component/LoadIcon";
 import "./index.css";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
   const [todoTitle, setTodoTitle] = useState("");
-
-  const [todoList, setTodoList] = useState(
-    localStorage.getItem("savedTodoList")
-      ? JSON.parse(localStorage.getItem("savedTodoList"))
-      : []
-  );
-
-  useEffect(() => {
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-  }, [todoList]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const removeTodo = (id) => {
     const newTodoList = todoList.filter((todo) => todo.id !== id);
@@ -24,6 +17,27 @@ function App() {
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
   };
+
+  useEffect(() => {
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          data: {
+            todoList: JSON.parse(localStorage.getItem("savedTodoList") || []),
+          },
+        });
+      }, 2000);
+    }).then((result) => {
+      setTodoList(result.data.todoList);
+      setIsLoading(false);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+    }
+  }, [isLoading, todoList]);
 
   return (
     //update from <div> element  to fragment <></>
@@ -36,6 +50,7 @@ function App() {
         setTodoTitle={setTodoTitle}
         onAddTodo={addTodo}
       />
+      {isLoading && <LoadIcon />}
       <TodoList onRemoveTodo={removeTodo} todos={todoList} />
     </>
   );
