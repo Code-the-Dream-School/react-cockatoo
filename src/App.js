7efe72d466ac6cf143ae285d57c65629a0448aa8
Said-laasri <saidlaasri3@gmail.com>
2023-03-20 15:38:52 +0000

    Add routes to Todoapp as default route and newtodo as /new route

--- src/App.js
+++ src/App.js
@@ -1,61 +1,16 @@
-import React, { useState, useEffect } from "react";
-import AddTodoForm from "./Component/AddTodoForm";
-import TodoList from "./Component/TodoList";
-import LoadIcon from "./Component/LoadIcon";
-import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TodoApp from "./Component/TodoApp";
+import NewTodo from "./Component/NewTodo";
 
-const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`;
-
-function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const options = {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
-      },
-    };
-
-    fetch(url, options)
-      .then((response) => response.json())
-
-      .then((response) => {
-        console.log(response.records);
-        setTodoList(response.records);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.warn(error);
-      });
-  }, []);
-
-  useEffect(() => {
-    if (!isLoading) {
-      localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-    }
-  }, [isLoading, todoList]);
-
-  const removeTodo = (id) => {
-    const newTodoList = todoList.filter((todo) => todo.id !== id);
-    setTodoList(newTodoList);
-  };
-
-  const addTodo = (newTodo) => {
-    setTodoList([...todoList, newTodo]);
-  };
+const App = () => {
   return (
-    //update from <div> element  to fragment <></>
-    <>
-      <header>
-        <h1>Todo List</h1>
-      </header>
-      <AddTodoForm onAddTodo={addTodo} />
-      {isLoading && <LoadIcon />}
-      <TodoList onRemoveTodo={removeTodo} todos={todoList} />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TodoApp />} />
+        <Route path="/new" element={<NewTodo />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
