12adba09eb897cedcd6259e9480a2bd84d3e2804
Said-laasri <saidlaasri3@gmail.com>
2023-04-20 02:34:11 +0000

    Add toggle button so the user can switch between ascending and descending sort order

--- src/Component/TodoApp.js
+++ src/Component/TodoApp.js
@@ -4,14 +4,16 @@ import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
 import { FiArrowRight } from "react-icons/fi";
 import LoadIcon from "./LoadIcon";
-import  style from './TodoApp.module.css';
-
+import style from "./TodoApp.module.css";
+// Title sorting
 const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default?view=Grid%20view&sort[0][field]=Title&sort[0][direction]=asc`;
-// const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default?sort[0][filed]=Title&sort[0][direction]=asc`;
+// createdTime sorting
+// const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default?view=Grid%20view&sort[0][field]=createdTime&sort[0][direction]=asc`;
 
 function TodoApp() {
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isAscending, setIsAscending] = useState(true);
 
   useEffect(() => {
     const options = {
@@ -23,16 +25,25 @@ function TodoApp() {
 
     fetch(url, options)
       .then((response) => response.json())
-
       .then((response) => {
-        // console.log(response.records);
+        //sort the todo list by title and direction based on the isAscending state
+        response.records.sort((objectA, objectB) => {
+          const direction = isAscending ? 1 : -1;
+          if (objectA.fields.Title < objectB.fields.Title) {
+            return direction;
+          }
+          if (objectA.fields.Title > objectB.fields.Title) {
+            return -direction;
+          }
+          return 0;
+        });
         setTodoList(response.records);
         setIsLoading(false);
       })
       .catch((error) => {
         console.warn(error);
       });
-  }, []);
+  }, [isAscending]);
 
   useEffect(() => {
     if (!isLoading) {
@@ -48,12 +59,19 @@ function TodoApp() {
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
   };
+
+  const toggleSortOrder = () => {
+    setIsAscending(!isAscending);
+  };
+
   return (
-    //update from <div> element  to fragment <></>
     <>
       <header>
         <h1 className={style.header}>Todo List</h1>
       </header>
+      <button onClick={toggleSortOrder}>
+        {isAscending ? "Sort Descending" : "Sort Ascending"}
+      </button>
       <AddTodoForm onAddTodo={addTodo} />
       {isLoading && <LoadIcon />}
       <TodoList onRemoveTodo={removeTodo} todos={todoList} />
