41ce2dd0ca2f212aae4bd3818e2784aba4f44997
azizullahaziz <aziz.h2949@gmail.com>
2023-05-03 15:55:54 +0430

    Lesson 4.1 Assignment

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react"
 import InputWithLabel from "./InputWithLabel"
+import PropTypes from "prop-types"
 function AddTodoForm({ onAddTodo }) {
   const [todoTitle, setTodoTitle] = useState("")
   function handleTitleChange(event) {
@@ -21,6 +22,10 @@ function AddTodoForm({ onAddTodo }) {
       </button>
     </form>
   )
+}
+
+AddTodoForm.propTypes = {
+  onAddTodo: PropTypes.func,
 }
 
 export default AddTodoForm
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -2,6 +2,7 @@ import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faTrash } from "@fortawesome/free-solid-svg-icons"
 import style from "./TodoListItem.module.css"
+import PropTypes from "prop-types"
 
 function TodoListItem({ todo, onRemoveTodo }) {
   return (
@@ -16,4 +17,7 @@ function TodoListItem({ todo, onRemoveTodo }) {
   )
 }
 
+TodoListItem.propTypes = {
+  onRemoveTodo: PropTypes.func.isRequired,
+}
 export default TodoListItem
